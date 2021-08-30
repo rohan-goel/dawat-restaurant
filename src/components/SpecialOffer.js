f334ab57f641c6bd015e2fa936da1bdc93190bfe
Rohan Goel <rohangoel107@gmail.com>
2021-08-30 15:35:25 +0530

    special package details done

--- src/components/SpecialOffer.js
+++ src/components/SpecialOffer.js
@@ -11,7 +11,7 @@ function SpecialOffer() {
         <img src="./dawat-images/promo_slide_1.jpg" alt="" className="promo1" />
         <div className="card">
           <h3> $40% off for 9'-12' pizza-Wed, Thu,Friday only</h3>
-          <p>Pizza 12 inch + Side Dish 1.5L Coke Bottle Only from 209,000</p>
+          <h4>Pizza 12 inch + Side Dish 1.5L Coke Bottle Only from 209,000</h4>
           <button className="order-now">ORDER NOW</button>
         </div>
       </div>
