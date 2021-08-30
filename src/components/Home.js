1ea7957b32a32ea71439ef66cafe0998e815105d
Rohan Goel <rohangoel107@gmail.com>
2021-08-30 14:09:08 +0530

    home screen details done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,29 @@ function Home() {
   return (
     <div className="first_page">
       <header className="header1">
-        <div className="front">
+        <div>
           <span>
-            <i class="far fa-envelope"></i>Email
+            <i class="far fa-envelope"></i>Email: Info@Restaurant.com
           </span>
           <span>
-            <i class="fas fa-phone-alt"></i>Telephone
+            <i class="fas fa-phone-alt"></i>Telephone: +18002345678
           </span>
         </div>
-        <div className="back">
+        <div>
           <i class="fab fa-facebook-f"></i>
           <i class="fab fa-twitter"></i>
           <i class="fab fa-behance"></i>
           <i class="fas fa-wifi fa-rotate-45"></i>
 
-          <button>Book A Ticket</button>
+          <button>BOOK A TICKET</button>
         </div>
       </header>
       <hr />
       <header className="header2">
-        <div className="front">
+        <div>
           <img src="./dawat-images/main_logo.png" alt="" />
         </div>
-        <div className="back">
+        <div className="nav_links">
           <a href="Home">Home</a>
           <a href="Home">About Us</a>
           <a href="Home">Offer</a>
@@ -35,10 +35,16 @@ function Home() {
           <a href="Home">Blog</a>
           <a href="Home">Gallery</a>
           <a href="Home">Contact</a>
-          <i class="fas fa-search"></i>
+          <a href="Home">
+            <i class="fas fa-search"></i>
+          </a>
         </div>
       </header>
-      <div className="slides">Our Menu See What's new today</div>
+      <div className="slides">
+        <span>Our Menu</span>
+        <span>SEE WHAT'S NEW TODAY</span>
+        <button>TODAY'S MENU</button>
+      </div>
     </div>
   );
 }
