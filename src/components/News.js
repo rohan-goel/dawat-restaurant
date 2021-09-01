b082804fba898fa32b82b5f9c127d55503e6683e
Rohan Goel <rohangoel107@gmail.com>
2021-09-01 17:52:05 +0530

    carousel added

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,13 @@
 import React from "react";
 import NewsCard from "./NewsCard";
+import Carousel from 'react-elastic-carousel';
 
 function News() {
+  const breakPoints = [
+  {width:500, itemToShow:3}
+]
+
+
   return (
     <div className="news" id="news_screen">
       <div className="heading">
@@ -9,6 +15,7 @@ function News() {
         <img src="./dawat-images/red.png" alt="" />
       </div>
       <div className="news-list">
+        <Carousel breakPoints={breakPoints}>
         <NewsCard
           photo="./dawat-images/blog-1.jpg"
           title="Restaurant Welcome Day Meetup"
@@ -21,6 +28,19 @@ function News() {
           photo="./dawat-images/blog-3.jpg"
           title="Home to get your home delivery?"
         />
+        <NewsCard
+          photo="./dawat-images/blog-1.jpg"
+          title="Restaurant Welcome Day Meetup"
+        />
+        <NewsCard
+          photo="./dawat-images/blog-2.jpg"
+          title="Restaurant first Day first reservation"
+        />
+        <NewsCard
+          photo="./dawat-images/blog-3.jpg"
+          title="Home to get your home delivery?"
+        />
+        </Carousel>
       </div>
     </div>
   );
